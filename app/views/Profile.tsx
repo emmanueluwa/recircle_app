75231cf5f9a05f9190d1ae577f17ee3ce9637a8e
emmanueluwa <emmanuel@fulodev.com>
2024-06-04 16:13:42 +0100

    adding user details to profile screen

--- app/views/Profile.tsx
+++ app/views/Profile.tsx
@@ -1,71 +1,42 @@
 import { FC, useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { AuthStackParamList } from "app/navigator/AuthNavigator";
-import { signInSchema, yupValidate } from "@utils/validator";
-import { showMessage } from "react-native-flash-message";
-import { runAxiosAsync } from "app/api/runAxiosAsync";
-import client from "app/api/client";
 import AvatarView from "@ui/AvatarView";
+import useAuth from "app/hooks/useAuth";
+import colours from "@utils/colours";
+import size from "@utils/size";
+import FormDivider from "./FormDivider";
 
 interface Props {}
 
-export interface SignInRes {
-  profile: {
-    id: string;
-    email: string;
-    name: string;
-    verified: boolean;
-    avatar?: string;
-  };
-  tokens: {
-    refresh: string;
-    access: string;
-  };
-}
-
 const Profile: FC<Props> = (props) => {
-  const [userInfo, setUserInfo] = useState({
-    email: "",
-    password: "",
-  });
+  const { authState } = useAuth();
+  const { profile } = authState;
 
-  const [busy, setBusy] = useState(false);
-
-  const { navigate } = useNavigation<NavigationProp<AuthStackParamList>>();
-
-  const handleChange = (email: string) => (text: string) => {
-    setUserInfo({ ...userInfo, [email]: text });
-  };
-
-  const handleSubmit = async () => {
-    const { values, error } = await yupValidate(signInSchema, userInfo);
-
-    if (error) return showMessage({ message: error, type: "danger" });
-
-    //set busy if api request is sent
-    setBusy(true);
-    const res = await runAxiosAsync<SignInRes>(
-      client.post("/auth/sign-in", values)
-    );
-
-    if (res) {
-      console.log(res);
-    }
-  };
-
-  const { email, password } = userInfo;
   return (
     <ScrollView contentContainerStyle={styles.container}>
       {/* Profile image and profile info */}
-      <AvatarView size={80} />
+      <View style={styles.profileContainer}>
+        <AvatarView uri={authState.profile?.avatar} size={80} />
+
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>{profile?.name}</Text>
+          <Text style={styles.email}>{profile?.email}</Text>
+        </View>
+      </View>
+
+      <FormDivider />
+
       {/* options for profile */}
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: { padding: size.padding },
+  profileContainer: { flexDirection: "row", alignItems: "center" },
+  profileInfo: { flex: 1, paddingLeft: size.padding },
+  name: { color: colours.primary, fontSize: 20, fontWeight: "bold" },
+  email: { color: colours.primary, paddingTop: 2 },
 });
 
 export default Profile;
