fd55faf7b5d3baf329b8278f1cc3190b065fe8f2
emmanueluwa <emmanuel@fulodev.com>
2024-06-05 13:22:27 +0100

    adding ability to get users listings

--- app/views/Listings.tsx
+++ app/views/Listings.tsx
@@ -1,18 +1,65 @@
-import { FC } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FC, useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Profile from "@views/Profile";
 import AppHeader from "@components/AppHeader";
 import BackButton from "@ui/BackButton";
+import useClient from "app/hooks/useClient";
+import { runAxiosAsync } from "app/api/runAxiosAsync";
 
 const Stack = createNativeStackNavigator();
 
 interface Props {}
 
+type Product = {
+  id: string;
+  name: string;
+  thumbnail?: string;
+  category: string;
+  price: number;
+  image?: string[];
+  date: Date;
+  description: string;
+  seller: {
+    id: string;
+    name: string;
+    avatar?: string;
+  };
+};
+
+type ListingResponse = {
+  products: Product[];
+};
+
 const Listings: FC<Props> = (props) => {
+  const [listings, setListings] = useState<Product[]>([]);
+  const { authClient } = useClient();
+
+  const fetchListings = async () => {
+    const res = await runAxiosAsync<ListingResponse>(
+      authClient.get("/product/listings")
+    );
+
+    if (res) {
+      setListings(res.products);
+    }
+  };
+
+  useEffect(() => {
+    fetchListings();
+  }, []);
+
   return (
     <View>
       <AppHeader backButton={<BackButton />} />
+
+      <FlatList
+        data={listings}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 };
