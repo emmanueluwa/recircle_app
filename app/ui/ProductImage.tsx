f5200027fc42d663c1d0d6c2b98762c4bf2f2bae
emmanueluwa <emmanuel@fulodev.com>
2024-06-05 14:05:29 +0100

    adding ability to see listings image and title on user listings screen

---
+++ app/ui/ProductImage.tsx
@@ -0,0 +1,30 @@
+import size from "@utils/size";
+import { FC } from "react";
+import { Dimensions, Image, Pressable, StyleSheet, Text } from "react-native";
+import colours from "@utils/colours";
+
+interface Props {
+  uri?: string;
+}
+
+// aspect ratio for image
+const { width } = Dimensions.get("screen");
+const imageWidth = width - size.padding * 2;
+const aspect = 16 / 9;
+
+const ProductImage: FC<Props> = ({ uri }) => {
+  return (
+    <Image
+      source={{ uri }}
+      style={styles.image}
+      resizeMethod="resize"
+      resizeMode="cover"
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  image: { width: imageWidth, height: imageWidth / aspect, borderRadius: 7 },
+});
+
+export default ProductImage;
--- app/views/Listings.tsx
+++ app/views/Listings.tsx
@@ -1,11 +1,13 @@
 import { FC, useEffect, useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Profile from "@views/Profile";
 import AppHeader from "@components/AppHeader";
 import BackButton from "@ui/BackButton";
 import useClient from "app/hooks/useClient";
 import { runAxiosAsync } from "app/api/runAxiosAsync";
+import size from "@utils/size";
+import ProductImage from "@ui/ProductImage";
 
 const Stack = createNativeStackNavigator();
 
@@ -50,28 +52,47 @@ const Listings: FC<Props> = (props) => {
   }, []);
 
   return (
-    <View>
+    <>
       <AppHeader backButton={<BackButton />} />
-
-      <FlatList
-        data={listings}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => {
-          return <Text>{item.name}</Text>;
-        }}
-      />
-    </View>
+      <View>
+        <FlatList
+          data={listings}
+          contentContainerStyle={styles.flatList}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => {
+            return (
+              <Pressable style={styles.listItem}>
+                <ProductImage uri={item.thumbnail} />
+                <Text style={styles.productName} numberOfLines={2}>
+                  {item.name}
+                </Text>
+              </Pressable>
+            );
+          }}
+        />
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: { flex: 1, padding: size.padding },
   innerContainer: {
     padding: 15,
     flex: 1,
   },
 
-  formContainer: { marginTop: 30 },
+  listItem: { paddingBottom: size.padding },
+  flatList: {
+    paddingBottom: 20,
+  },
+
+  productName: {
+    fontWeight: "700",
+    fontSize: 20,
+    letterSpacing: 1,
+    paddingTop: 10,
+  },
 });
 
 export default Listings;
