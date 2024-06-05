81e6064fe0b375c12b190399450504b3926b45bf
emmanueluwa <emmanuel@fulodev.com>
2024-06-05 17:52:41 +0100

    adding styling to image slider

--- app/components/ImageSlider.tsx
+++ app/components/ImageSlider.tsx
@@ -3,35 +3,48 @@ import ProductImage from "@ui/ProductImage";
 import colours from "@utils/colours";
 import size from "@utils/size";
 import { FC, useState } from "react";
-import { FlatList, Pressable, StyleSheet, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 
 interface Props {
   images?: string[];
 }
 
 const ImageSlider: FC<Props> = ({ images }) => {
-  const { goBack, canGoBack } = useNavigation();
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <View style={styles.container}>
       <FlatList
+        contentContainerStyle={styles.flatList}
         data={images}
         renderItem={({ item }) => <ProductImage uri={item} />}
         horizontal
         showsHorizontalScrollIndicator={false}
         pagingEnabled
       />
+      <View style={styles.indicator}>
+        <Text style={styles.indicatorText}>
+          {activeIndex + 1}/{images?.length}
+        </Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
+  container: {},
+  flatList: { position: "relative" },
+  indicatorText: { color: colours.white, fontWeight: "600" },
+  indicator: {
+    position: "absolute",
+    width: 35,
+    height: 25,
+    backgroundColor: colours.backDropDark,
+    bottom: 10,
+    right: 10,
+    justifyContent: "center",
     alignItems: "center",
-    justifyContent: "space-between",
-    padding: size.padding,
+    borderRadius: 5,
   },
 });
 
 export default ImageSlider;
-ImageSlider;
--- app/utils/colours.ts
+++ app/utils/colours.ts
@@ -2,8 +2,9 @@ const primary = "#1C1C1F";
 const inActive = "#DCE4F2";
 const active = "#0081f1";
 const white = "#FFFFFF";
-const backDrop = "rgba(0,0,0,0.5)";
+const backDrop = "rgba(225,225,225,0.5)";
+const backDropDark = "rgba(0,0,0,0.5)";
 
-const colours = { primary, inActive, white, backDrop, active };
+const colours = { primary, inActive, white, backDrop, active, backDropDark };
 
 export default colours;
