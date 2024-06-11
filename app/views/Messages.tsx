28c0c70c7eeec730603c841a1392cb6c2abb3e39
emmanueluwa <emmanuel@fulodev.com>
2024-06-12 00:18:07 +0100

    adding ability to get last chats from backend api

--- app/views/Messages.tsx
+++ app/views/Messages.tsx
@@ -1,15 +1,49 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Profile from "@views/Profile";
 import AppHeader from "@components/AppHeader";
 import BackButton from "@ui/BackButton";
+import EmptyView from "@ui/EmptyView";
+import useClient from "app/hooks/useClient";
+import { runAxiosAsync } from "app/api/runAxiosAsync";
 
 const Stack = createNativeStackNavigator();
 
 interface Props {}
 
 const Messages: FC<Props> = (props) => {
+  const { authClient } = useClient();
+  const chats = [];
+
+  const fetchLastChats = async () => {
+    const res = await runAxiosAsync<{
+      chats: {
+        id: string;
+        lastMessage: string;
+        timestamp: Date;
+        unreadChatCounts: number;
+        peerProfile: { id: string; name: string; avatar?: string };
+      };
+    }>(authClient("/conversation/last-chats"));
+
+    if (res) {
+      console.log(res.chats);
+    }
+  };
+
+  useEffect(() => {
+    fetchLastChats();
+  }, []);
+
+  if (!chats.length)
+    return (
+      <>
+        <AppHeader backButton={<BackButton />} />
+        <EmptyView title="There are no messages." />
+      </>
+    );
+
   return (
     <View>
       <AppHeader backButton={<BackButton />} />
