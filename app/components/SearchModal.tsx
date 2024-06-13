5b9c7fdd9c144822e98d820ca4ed9f59a3259336
emmanueluwa <emmanuel@fulodev.com>
2024-06-13 15:56:30 +0100

    adding keyboard api for search with keyboard

--- app/components/SearchModal.tsx
+++ app/components/SearchModal.tsx
@@ -1,7 +1,7 @@
 import { Ionicons } from "@expo/vector-icons";
 import colours from "@utils/colours";
 import size from "@utils/size";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import {
   FlatList,
   Modal,
@@ -15,6 +15,7 @@ import {
 } from "react-native";
 import SearchBar from "./SearchBar";
 import { SafeAreaView } from "react-native-safe-area-context";
+import EmptyView from "@ui/EmptyView";
 
 interface Props {
   visible: boolean;
@@ -22,32 +23,54 @@ interface Props {
 }
 
 const searchResults = [
-  { id: 1, name: "Bentley Bentayga" },
-  { id: 2, name: "Mercedes-Benz G-Class" },
-  { id: 3, name: "Porsche Cayenne" },
-  { id: 4, name: "Lamborghini Urus" },
-  { id: 5, name: "Audi Q8" },
-  { id: 6, name: "BMW X7" },
-  { id: 7, name: "Range Rover Velar" },
-  { id: 8, name: "Tesla Model X" },
-  { id: 9, name: "Maserati Levante" },
-  { id: 10, name: "Rolls-Royce Cullinan" },
-  { id: 11, name: "Great Dane" },
-  { id: 12, name: "Mastiff" },
-  { id: 13, name: "Saint Bernard" },
-  { id: 14, name: "Bernese Mountain Dog" },
-  { id: 15, name: "Newfoundland" },
-  { id: 16, name: "Rottweiler" },
-  { id: 17, name: "Irish Wolfhound" },
-  { id: 18, name: "Leonberger" },
-  { id: 19, name: "Great Pyrenees" },
-  { id: 20, name: "Anatolian Shepherd" },
+  // { id: 1, name: "Bentley Bentayga" },
+  // { id: 2, name: "Mercedes-Benz G-Class" },
+  // { id: 3, name: "Porsche Cayenne" },
+  // { id: 4, name: "Lamborghini Urus" },
+  // { id: 5, name: "Audi Q8" },
+  // { id: 6, name: "BMW X7" },
+  // { id: 7, name: "Range Rover Velar" },
+  // { id: 8, name: "Tesla Model X" },
+  // { id: 9, name: "Maserati Levante" },
+  // { id: 10, name: "Rolls-Royce Cullinan" },
+  // { id: 11, name: "Great Dane" },
+  // { id: 12, name: "Mastiff" },
+  // { id: 13, name: "Saint Bernard" },
+  // { id: 14, name: "Bernese Mountain Dog" },
+  // { id: 15, name: "Newfoundland" },
+  // { id: 16, name: "Rottweiler" },
+  // { id: 17, name: "Irish Wolfhound" },
+  // { id: 18, name: "Leonberger" },
+  // { id: 19, name: "Great Pyrenees" },
+  // { id: 20, name: "Anatolian Shepherd" },
 ];
 
 const SearchModal: FC<Props> = ({ visible, onClose }) => {
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
   const handleClose = () => {
     onClose(!visible);
   };
+
+  useEffect(() => {
+    const keyShowEvent =
+      Platform.OS === "ios" ? "keyboardWillShow" : "keyboardDidShow";
+    const keyHideEvent =
+      Platform.OS === "ios" ? "keyboardWillHide" : "keyboardDidHide";
+
+    const keyShowListener = Keyboard.addListener(keyShowEvent, (evt) => {
+      setKeyboardHeight(evt.endCoordinates.height + 50);
+    });
+
+    const keyHideListener = Keyboard.addListener(keyHideEvent, (evt) => {
+      setKeyboardHeight(0);
+    });
+
+    return () => {
+      keyShowListener.remove();
+      keyHideListener.remove();
+    };
+  }, []);
 
   return (
     <Modal animationType="fade" onRequestClose={handleClose} visible={visible}>
@@ -68,16 +91,19 @@ const SearchModal: FC<Props> = ({ visible, onClose }) => {
           </View>
 
           {/* search suggestions */}
-          <FlatList
-            data={searchResults}
-            renderItem={({ item }) => (
-              <Pressable>
-                <Text style={styles.suggestionListItem}>{item.name}</Text>
-              </Pressable>
-            )}
-            keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={styles.suggestionList}
-          />
+          <View style={{ paddingBottom: keyboardHeight }}>
+            <FlatList
+              data={searchResults}
+              renderItem={({ item }) => (
+                <Pressable>
+                  <Text style={styles.suggestionListItem}>{item.name}</Text>
+                </Pressable>
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              contentContainerStyle={styles.suggestionList}
+              ListEmptyComponent={<EmptyView title="No results found..." />}
+            />
+          </View>
         </View>
       </SafeAreaView>
     </Modal>
@@ -92,7 +118,7 @@ const styles = StyleSheet.create({
   innerContainer: { padding: size.padding },
   header: { flexDirection: "row", alignItems: "center" },
   searchBar: { flex: 1, marginLeft: size.padding },
-  suggestionList: { paddingHorizontal: size.padding },
+  suggestionList: { padding: size.padding },
   suggestionListItem: {
     color: colours.primary,
     fontWeight: "600",
